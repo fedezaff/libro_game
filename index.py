4943b84f36bd5320561bfbecb35844d62c1fd6d4
fedezaff <73648840+fedezaff@users.noreply.github.com>
2024-03-17 09:37:31 +0100

    Update index.py
    try other dimension for the button

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ pagina_iniziale.configure(background="#E1C699")
 titolo = tk.Label(pagina_iniziale, text="BAND\nil libro-game", font=("Helvetica", 20), background=v.bg, foreground=v.colore_titolo, pady=10)
 titolo.pack(side=tk.TOP)
 
-button = tk.Button(text="AVVIA", command=partial(creazione_pg.pagina_nome_pg, pagina_iniziale), background=v.bg, foreground=v.colore_titolo,)
+button = tk.Button(text="AVVIA", command=partial(creazione_pg.pagina_nome_pg, pagina_iniziale), font=("Helvetica", 20), background=v.bg, foreground=v.colore_titolo, height=2, width=20)
 button.pack()
 
 if __name__ == "__main__":    
